ec38aee7c0829b16e7614d106c5bf003355cd21e
Sangyups <feels4637@gmail.com>
2022-05-30 14:19:10 +0900

    feat: basic API key library test

--- example/urls.py
+++ example/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from example.views import UserListView
+from example.views import UserListView, LoginView
 
-urlpatterns = [path("users/", UserListView.as_view())]
+urlpatterns = [
+    path("users/", UserListView.as_view()),
+    path("auth/signin/", LoginView.as_view()),
+]
--- example/views.py
+++ example/views.py
@@ -1,12 +1,33 @@
+from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
-from rest_framework import generics, mixins
+from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_api_key.models import APIKey
+from rest_framework_api_key.permissions import HasAPIKey
 
-from example.permissions import HasAPIKeyOrReadOnly
 from example.serializers import UserSerializer
 
 
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [HasAPIKeyOrReadOnly]
+    permission_classes = [HasAPIKey]
+
+
+class LoginView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def post(self, request):
+        queryset = self.get_queryset()
+        user = queryset.get(username=request.data["username"])
+        if not check_password(request.data["password"], user.password):
+            raise PermissionError("Invalid password")
+        try:
+            api_key = APIKey.objects.get(name=request.data["username"])
+            api_key.delete()
+            api_key, key = APIKey.objects.create_key(name=request.data["username"])
+        except APIKey.DoesNotExist:
+            api_key, key = APIKey.objects.create_key(name=request.data["username"])
+
+        return Response({"api_key": key}, status=status.HTTP_200_OK)

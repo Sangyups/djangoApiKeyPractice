56a1d9236204b7e7af3641ed00053a344893f138
Sangyups <feels4637@gmail.com>
2022-06-08 17:44:29 +0900

    feat: add custom manager getting user from api key

--- example/models.py
+++ example/models.py
@@ -1,9 +1,27 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from rest_framework_api_key.models import AbstractAPIKey
+from rest_framework_api_key.models import AbstractAPIKey, BaseAPIKeyManager
+
+
+class CustomAPIKeyManager(BaseAPIKeyManager):
+    def get_user_from_key(self, key: str) -> "AbstractAPIKey":
+        prefix, _, _ = key.partition(".")
+        queryset = self.get_usable_keys()
+
+        try:
+            api_key = queryset.select_related("user").get(prefix=prefix)
+        except self.model.DoesNotExist:
+            raise  # For the sake of being explicit.
+
+        if not api_key.is_valid(key):
+            raise self.model.DoesNotExist("Key is not valid.")
+        else:
+            return api_key.user
 
 
 class UserAPIKey(AbstractAPIKey):
+    objects = CustomAPIKeyManager()
+
     user = models.OneToOneField(
         get_user_model(), on_delete=models.CASCADE, related_name="api_key"
     )
